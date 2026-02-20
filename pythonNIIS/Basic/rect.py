97f839c0cd93f10927f25ee05cb38c13f83722fc
vickyarch2107 <dasp72208@gmial.com>
2026-02-20 12:30:39 +0530

    Rectangle and Square

---
+++ pythonNIIS/Basic/rect.py
@@ -0,0 +1,15 @@
+"""WAP take rectaangle length and breadth from keybard and display length breadth area perimeter"""
+
+
+
+print("Enter rectangle length:")
+l=int(input())
+print("Enter rectangle length:")
+b=int(input())
+ar=l*b
+pr=2*(l+b)
+print("length",l)
+print("breadth",b)
+print("Area",ar)
+print("Perimeter",pr)
+
---
+++ pythonNIIS/Basic/square.py
@@ -0,0 +1,7 @@
+print("Enter side of square")
+s=int(input())
+ar=s**2
+pr=2*s
+print("Side",s)
+print("Area",ar)
+print("Perimeter",pr)
